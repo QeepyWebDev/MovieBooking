a6b72e16c834be4a2b58f695e78d40cc8745a38f
QeepyWebDev <145613539+QeepyWebDev@users.noreply.github.com>
2024-11-09 00:27:58 +0100

    Moved code to App.vue

--- movie-booking/vite.config.ts
+++ movie-booking/vite.config.ts
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +9,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  base: './',  // Use relative paths for assets when deploying to a subfolder
+});
